1b3cbff4c19252dd285d9925af9f3ee23823989e
leandrolimadeveloper <leandroslimasr@gmail.com>
2021-11-23 16:53:46 -0300

    Sorting questions from the last to the first

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.use(express.json())
 
 // Rotas
 app.get('/', (req, res) => {
-    Pergunta.findAll({raw: true}).then(perguntas => {
+    Pergunta.findAll({raw: true, order:[
+        ['id', 'DESC'] 
+    ]}).then(perguntas => {
         res.render('index', {
             perguntas: perguntas
         })    
