31ffbfb5b7400c7eb3c3b33c1ed8c4616e7b2c38
leandrolimadeveloper <leandroslimasr@gmail.com>
2021-11-22 23:11:12 -0300

    Set ejs partials and body-parser

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+// Body-Parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Rotas
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -12,41 +17,12 @@ app.get('/pergunta', (req, res) => {
     res.render('perguntar')
 })
 
-app.get('/', (req, res) => {
-    res.render('sobre/informacao')
-})
-
-app.get('/categorias/:distro/:versao', (req, res) => {
-    var distro = req.params.distro
-    var versao = req.params.versao
-    res.render('categoria', {
-        distro: distro,
-        versao: versao
-    })
-})
-
-app.get('/perfil', (req, res) => {
-    var status = false
-    var nome = 'Leandro Lima'
-    var linguagem = 'JS'
-    var redesSociais = [
-        {nome: 'Facebook'},
-        {nome: 'Twitter'}, 
-        {nome: 'Instagram'},
-    ]
-    res.render('perfil', {
-        nome: nome,
-        linguagem: linguagem,
-        empresa: 'Ask Linux',
-        pais: 'Brasil',
-        seguidores: 3500,
-        redes: redesSociais,
-        situacao: status,
-        distros: [{nome: 'Ubuntu', versao: '22.10'}, {nome: 'Linux Mint', versao: 22.2}, {nome: 'Fedora'}]
-    })
+app.post('/salvarpergunta', (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send('Formulário recebido<br>Título: ' + titulo + '<br>Descrição: ' + descricao)
 })
 
 app.listen(3000, 'localhost', () => {
     console.log('App rodando!')
 })
-
